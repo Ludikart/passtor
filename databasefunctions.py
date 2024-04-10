f9e327e9ed1904064c8500d94f2517c5f1466bd2
Ludikart <lassi.alm@gmail.com>
2024-04-10 12:10:28 +0300

    Removed old stuff, added hashing, got click to work and added .gitignore

--- databasefunctions.py
+++ databasefunctions.py
@@ -1,4 +1,14 @@
 import sqlite3
+import argon2
+import click
+from Crypto.Hash import SHA256
+
+DATABASE_FILE = "passtorDatabase.db"
+INIT_SCRIPT = "schema.sql"
+
+@click.group()
+def cli():
+    pass
 
 def addListing(listingType, userName, passWord):
     # Save to database
@@ -16,28 +26,34 @@ def removeUser(listingType, userName):
 def getRecords(userID):
     pass
 
-def newUser(userHash, passwordHash):
-    db = sqlite3.connect("passtorDatabase.db", detect_types=sqlite3.PARSE_DECLTYPES)
+@cli.command()
+def hello():
+    click.echo("Hello")
+    print("hello")
+
+@cli.command()
+@click.option('--user', prompt="Username")
+@click.option('--password', prompt="Password")
+def newuser(user, password):
+    db = sqlite3.connect(DATABASE_FILE, detect_types=sqlite3.PARSE_DECLTYPES)
     cur = db.cursor()
+
+    usernameHasher = SHA256.new()
+    userHash = usernameHasher.update(bytes(user, 'UTF-8'))
+
+    passwordHasher = argon2.PasswordHasher()
+    passwordHash = passwordHasher.hash(password)
+
     tup = (userHash, passwordHash)
     cur.execute("INSERT INTO user (username, pass) VALUES (?,?)", tup)
     db.commit()
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+@cli.command()
+def initdb():
+    db = sqlite3.connect(DATABASE_FILE, detect_types=sqlite3.PARSE_DECLTYPES)
+    cur = db.cursor()
+    with open(INIT_SCRIPT) as script:
+        cur.executescript(script.read())
 
-    return g.db
-
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-newUser("lassi2", "abc4137492ac")
+if __name__ == '__main__':
+    cli()
