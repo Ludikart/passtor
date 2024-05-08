6eb2390820b391c5e7793b9c5ea2d67a44f29070
Ludikart <lassi.alm@gmail.com>
2024-05-08 10:47:04 +0300

    Fixed some problems with reading and writing the database

--- clientv2.py
+++ clientv2.py
@@ -4,19 +4,27 @@ import requests
 import json
 from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Hash import SHA512
+from Crypto.Hash import SHA512, SHA256
 
 configFile = "passtorconfig"
 session_cookie = ""
 username_hash = ""
 
 global database
+database = {"test": ("test1", "test2")}
+
+def register(address, username, password):
+    pass
 
 def authenticate(address, username, masterPass):
     session = requests.Session()
     loginData = {'username': username, 'password': masterPass}
     loginData = json.dumps(loginData)
-    resp = session.post(address + "/login", data=loginData, headers={'Content-Type': 'application/json'})
+    try:
+        resp = session.post(address + "/login", data=loginData, headers={'Content-Type': 'application/json'})
+    except:
+        print("Couldn't connect to server")
+        return False
     
     global session_cookie
     print(resp.cookies['session'])
@@ -26,27 +34,30 @@ def authenticate(address, username, masterPass):
 
 def listRecords():
     global database
-    print(database.keys)
+    print(database.keys())
 
-def newrecord():
+def newRecord():
     global database
     key = input("Type of record: ")
+    if(key in database):
+        print("Record already exists, please use c-option or use different record type")
+        return
     user = input("Username: ")
     password = getpass.getpass("Password: ")
-    if(input("Are you sure you want to add this?[y/n]" == y)):
-        database.append(item)
+    if(input("Are you sure you want to add this?[y/n]") == 'y' and key not in database):
+        database[key] = (user, password)
     return
 
 def logout(onionAddress):
     exit()
 
-def get_db(onionAddress, filename, encryptionkey):
+def get_db(onionAddress, loggedin, filename, encryptionkey):
     # If logged in, check update from server
     # Read database file
     # Decrypt
 
     global database
-    with open(filename, "wb") as file:
+    with open(filename, "r") as file:
         filedata = file.read()
         # decrypt here
         database = json.loads(filedata)
@@ -55,17 +66,17 @@ def get_db(onionAddress, filename, encryptionkey):
     return
 
 # Add encryption key as variable
-def save_database(onionAddress, dbfileName, encryptionkey):
+def save_database(onionAddress, dbfileName, encryptionkey, loggedin):
     # If logged in, send updated database file
     # else show error message
     # 
     global database
-    with open(dbfilename, 'wb') as file:
+    with open(dbfileName, 'wb') as file:
         datastream = json.dumps(database)
         # Encryption here
         file.write(bytes(datastream, 'utf-8'))
     
-    if (logged_in):
+    if (loggedin):
         with open(dbfilename, 'rb') as file:
             response = requests.post(onionAddress, files={'db': file})
     else:
@@ -73,9 +84,9 @@ def save_database(onionAddress, dbfileName, encryptionkey):
 
     return
 
-def get_config():
+def get_config(usernamehash):
     try:
-        with open(configFile, "r") as config:
+        with open(usernamehash, "r") as config:
             onionAddress = config.readline()
             privateKey = config.readline()
     except Exception:
@@ -94,16 +105,16 @@ def main():
     filename = "encrypteddb"
 
     logged_in = False
-    onionAddress, privateKey = get_config()
     usernameHasher = SHA256.new()
     usernameHasher.update(bytes(input("Username: "), "UTF-8"))
     username_hash = usernameHasher.hexdigest()
     password = getpass.getpass("Password: ")
+    onionAddress, privateKey = get_config(username_hash)
 
     logged_in = authenticate(onionAddress, username_hash, password)
     
     encryption_key = derivekey(password, privateKey)
-    database = get_db(encryption_key, logged_in)
+    database = get_db(onionAddress, logged_in, filename, encryption_key)
 
         # Basic command line interface
     while True:
